5656bac0bcc2745ad77ba1a4dbfed6b2dd845116
Minified <gora.pub@gmail.com>
2016-01-05 20:01:17 +0100

    msg

--- index.js
+++ index.js
@@ -12,22 +12,37 @@ server.listen(port)
 console.log("http server listening on %d", port)
 
 var clients = [];
+var history = [];
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  clients.push(ws);
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  let clindex = clients.push(ws);
+  // var id = setInterval(function() {
+  //   ws.send(JSON.stringify(new Date()), function() {  })
+  // }, 1000)
 
   console.log("websocket connection open")
   ws.on("message",function(msg){
     console.log('message: ',msg);
+    var ms;
+    try{
+      ms = JSON.parse(msg)
+    }else{
+      ms = {};
+      return;
+    }
+    history.push(msg);
+    history = history.slice(-100);
+    for(let client of clients){
+      client.send(msg);
+    }
+
   })
   ws.on("close", function() {
     console.log("websocket connection close")
-    clearInterval(id)
+    // clearInterval(id)
+    clients.splice(clindex,1)
   })
 })
